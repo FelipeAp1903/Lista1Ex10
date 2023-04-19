8a834b199207fc558e9fc611c4747202104f0772
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 17:40:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotação;
+            double dolar;
+            double reais;
+
+            Console.Write("coloque a cotação do dolar:");
+            cotação = double.Parse(Console.ReadLine());
+            Console.Write("Qual é o valor em dolares:");
+            dolar = double.Parse(Console.ReadLine());
+            reais = dolar * cotação;
+            Console.WriteLine("O valor em reais é:{0}R$" ,reais);
+        }
+    }
+}
